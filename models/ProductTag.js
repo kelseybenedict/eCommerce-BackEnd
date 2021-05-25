9758e4bd705baca7b73a3478b68990777164c7e9
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-05-25 10:04:59 -0600

    created product tag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER, // setting type integer
+      allowNull: false, // can't be null 
+      primaryKey: true, // set as primary key
+      autoIncrement: true, // uses autoincrement
+    },
+    product_id: {
+      type: DataTypes.INTEGER, // setting type integer
+      references: { // references product model's id
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, // setting type integer
+      references: { // references tag model's id
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
