225beb0797a0172c092ec7257ec5dfe4405753d9
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-05-25 10:02:54 -0600

    product model complete

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER, // setting type integer
+      allowNull: false, // can't be null
+      primaryKey: true, // set primary key
+      autoIncrement: true, // uses autoincrement
+    },
+    product_name: {
+      type: DataTypes.STRING, // setting type string
+      allowNull: false, // can't be null
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2), // setting type decimal
+      allowNull: false, // can't be null
+      validate: { // validation to check for decimal
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER, // setting type integer
+      allowNull: false, // can't be null
+      defaultValue: 10, // setting default value
+      validate: { // validation to check for numeric value
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER, // setting type integer
+      allowNull: true, // can be null
+      references: { // references category model's id
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
