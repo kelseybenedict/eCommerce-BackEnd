a20b99a47146842b71c6e20c39606894a5e98929
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-05-25 09:47:21 -0600

    set up index.js in models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  // foreign key set up
+  foreignKey = "category_id",
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+})
 
 module.exports = {
   Product,
